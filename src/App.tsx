f8c26e5366100fc9c5ab19a229bc86875be46402
gvillacreses <117663512+gvillacreses@users.noreply.github.com>
2025-07-09 15:54:28 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
+import SelectorUI from './components/SelectorUI';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-       <Grid container spacing={5} justifyContent="center" alignItems="center"container spacing={5} justifyContent="center" alignItems="center">
+    
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
 
-         {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
-
-         {/* Alertas */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
-
-         {/* Selector */}
-         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
-
-         {/* Indicadores */}
-         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
-
-         {/* Gráfico */}
-         <Grid size={{ xs: 12, md: 6 }}>Elemento: Gráfico</Grid>
-
-         {/* Tabla */}
-         <Grid size={{ xs: 12, md: 6 }}>Elemento: Tabla</Grid>
-
-         {/* Información adicional */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
-
+      {/* Encabezado */}
+      
+      <Grid size={{ xs: 12, md: 12 }} sx={{ display: {xs: 'none', md: 'block'} }}>
+        Elemento: Encabezado
+        <HeaderUI/>
       </Grid>
+
+      {/* Alertas */}
+      <Grid size={{ xs: 12, md: 12 }} sx={{ display: {xs: 'none', md: 'block'} }} container justifyContent="right" alignItems="center">
+        Elemento: Alertas
+        <AlertUI description="No se preveen lluvias"/>
+      </Grid>
+
+      {/* Selector */}
+      <Grid size={{ xs: 12, md: 3 }} sx={{ display: {xs: 'none', md: 'block'} }}>
+        Elemento: Selector
+        <SelectorUI />
+      </Grid>
+
+      {/* Indicadores */}
+      <Grid size={{ xs: 12, md: 9 }} sx={{ display: {xs: 'none', md: 'block'} }}>
+        Elemento: Indicadores
+      </Grid>
+
+      {/* Gráfico */}
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: {xs: 'none', md: 'block'} }}>
+        Elemento: Gráfico
+      </Grid>
+
+      {/* Tabla */}
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: {xs: 'none', md: 'block'} }}>
+        Elemento: Tabla
+      </Grid>
+
+      {/* Información adicional */}
+      <Grid size={{ xs: 12, md: 12 }} sx={{ display: {xs: 'none', md: 'block'} }}>
+        Elemento: Información adicional
+      </Grid>
+
+    </Grid>
       
 
-    </>
   )
 }
 
