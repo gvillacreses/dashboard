5fbecdfd453f15e4215433831b58f4003889568a
carrbrus <139665813+carrbrus@users.noreply.github.com>
2025-07-11 07:33:45 +0000

    cambios guia 14

--- src/components/SelectorUI.tsx
+++ src/components/SelectorUI.tsx
@@ -1,13 +1,19 @@
+import React, { useState } from 'react';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { type SelectChangeEvent } from '@mui/material/Select';
 
 export default function SelectorUI() {
-    
+    const [cityInput, setCityInput] = useState<string>('');
+
     const handleChange = (event: SelectChangeEvent<string>) => {
-        alert(event.target.value)
+        setCityInput(event.target.value);
     };
+
+    // Función para capitalizar la primera letra
+    const capitalize = (text: string) =>
+        text.charAt(0).toUpperCase() + text.slice(1);
 
     return (
         <FormControl fullWidth>
@@ -15,13 +21,23 @@ export default function SelectorUI() {
             <Select
                 labelId="city-select-label"
                 id="city-simple-select"
-                label="Ciudad" onChange={handleChange} >
-                <MenuItem disabled><em>Seleccione una ciudad</em></MenuItem>
+                label="Ciudad"
+                value={cityInput}
+                onChange={handleChange}
+            >
+                <MenuItem disabled value="">
+                    <em>Seleccione una ciudad</em>
+                </MenuItem>
                 <MenuItem value={"guayaquil"}>Guayaquil</MenuItem>
                 <MenuItem value={"quito"}>Quito</MenuItem>
                 <MenuItem value={"manta"}>Manta</MenuItem>
                 <MenuItem value={"cuenca"}>Cuenca</MenuItem>
             </Select>
+            {cityInput && (
+                <p>
+                    Información del clima en <b>{capitalize(cityInput)}</b>
+                </p>
+            )}
         </FormControl>
-   )
+    )
 }
