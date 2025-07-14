0674ea41468ccdf74e3170ccb46a177452b4fc63
carrbrus <139665813+carrbrus@users.noreply.github.com>
2025-07-14 14:48:13 +0000

    implementacion PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/dashboard',
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+         registerType: 'autoUpdate',
+         devOptions: {
+            enabled: true
+         }
+      })
+    ]
 })
